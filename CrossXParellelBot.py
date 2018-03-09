d44e65b5b122e3f4f98b81d003e88005459f6f04
nickfrosst <nickfrosst@gmail.com>
2018-03-09 00:43:46 -0500

    updated bot

--- CrossXParellelBot.py
+++ CrossXParellelBot.py
@@ -6,22 +6,24 @@ from prettySleep import pretty_sleep
 import praw
 import urllib2, re
 import Image
+#from PIL import Image
 import os,sys
 import image_slicer
 import json
 import time
+import string
 
 AGENT_NAME = "CrossXParallel"
 
 TEMP_NAME = "temp.jpg"
 
 TARGET_SUB = "parallelview"
-#TARGET_SUB = "reddit_api_test"
+TARGET_SUB = "reddit_api_test"
 SOURCE_SUB = "crossview"
 
-SEARCH_LIMIT = 50
+SEARCH_LIMIT = 20
 
-POST_LIMIT = 5
+POST_LIMIT = 1
 
 ImgurConfig = {
         'album': "OWRXY",
@@ -55,41 +57,56 @@ downloaders = [(flickr_download,'.*www\.flickr\.com.*'),
 
 imgur_client = ImgurClient(auth.client_id, auth.client_secret, auth.access_token, auth.refresh_token)
 
-user_agent = (AGENT_NAME)
-r = praw.Reddit(user_agent = user_agent)
-r.login(auth.reddit_username, auth.reddit_password)
+reddit = praw.Reddit(client_id=auth.reddit_client_id,
+                client_secret=auth.reddit_secret,
+                user_agent=AGENT_NAME,
+                username = auth.reddit_username,
+                password = auth.reddit_password)
 
-subreddit = r.get_subreddit(SOURCE_SUB)
+if not reddit.read_only:
+    print("obtained non read only reddit")
+
+subreddit = reddit.subreddit(SOURCE_SUB)
 
 submitted = 0
-for submission in subreddit.get_hot(limit = SEARCH_LIMIT):
+for submission in subreddit.hot(limit = SEARCH_LIMIT):
     if submitted == POST_LIMIT:
         break
         
     for downloader,patern in downloaders:
-        if not posted.has_key(submission.url) and re.match(patern,submission.url):
+        s = str(submission.title.encode("utf-8"))
+        if not posted.has_key(submission.url) and s.lower().find("r/parallelview") == -1 and re.match(patern,submission.url):
             print ("matched " + patern)
             downloader(submission.url, TEMP_NAME)
             
             flip(TEMP_NAME)
             
-            ImgurConfig['title'] = str(submission.title)
-            ImgurConfig['description'] = "parallel view version - original by " + str(submission.author.name) + " - " + str(submission.short_link) + " - " + str(submission.url)  
+            
+            ImgurConfig['title'] = filter(lambda x: x in string.printable, s)
+            print (ImgurConfig['title'])
+                
+            ImgurConfig['description'] = "parallel view version - original by " + str(submission.author.name) + " - " + str(submission.shortlink) + " - " + str(submission.url)  
             response = imgur_client.upload_from_path(TEMP_NAME, config=ImgurConfig, anon=False)
             
             while True:
                 try:
-                    print(str(response['link'][:-4]))
-                    reddit_post = r.submit(TARGET_SUB, "[XPost CrossView] " + str(submission.title) + " by " + str(submission.author.name), url=str(response['link'][:-4]))
+                    print(str(response['link'][:-4]))     
+                    print ("posting")
+                    title = "[Converted XPost CrossView] " + s + " by u/" + str(submission.author.name)
+                    reddit_post = reddit.subreddit(TARGET_SUB).submit( title, url=str(response['link'][:-4]))
                     break
-                except:
-                    e = sys.exc_info()[0]
-                    print(e)
-                    pretty_sleep(600)
+                except praw.exceptions.APIException as e:
+                    if e.field == 'ratelimit':
+                        print(e.message)
+                        m = re.search("\d+",e.message)
+                        pretty_sleep(60 * int(m.group(0)))
+                    else:
+                        raise NameError('GiveUp')
+                        
                     
             
-            reddit_post.add_comment('''Hello, I am a bot :) \n\n I cross post from r/crossview \n\n ''' + ImgurConfig['description'])        
-                    
+            reddit_post.reply('''Hello, I am a bot :) \n\n I cross post from r/crossview \n\n ''' + ImgurConfig['description'])        
+            print ("adding comment")
             posted[submission.url] = reddit_post.short_link
             print("submited " + posted[submission.url])
             submitted += 1 
@@ -97,4 +114,3 @@ for submission in subreddit.get_hot(limit = SEARCH_LIMIT):
             #write hash to disc
             json.dump(posted, open("postedHash.txt",'w'))
             break
-
